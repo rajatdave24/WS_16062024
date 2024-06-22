e8588efc3de9b23258d9570a2996ef8149068737
rajat <rajatdave24@gmail.com>
2024-06-22 17:18:28 +0530

    App.js commit 22/06/2024

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export const App = () => {
   // });
   
   return (
-    <>
+    <div className='app1'>
       <div>
         <div className={(menu) ? 'menuContainerClicked' : 'menuContainer'}id="menuContainer">
         <img src="https://iskconmumbai.com/images/logo-black.png" alt="Logo" className="logo"/>
@@ -37,16 +37,31 @@ export const App = () => {
           <div className={(menu) ? 'menuIconClicked' : 'menuIcon'} onClick = {() => { setMenu(prevState=> !prevState); console.log(menu, document.getElementById
             ('ul').className)}}>&#9776;</div >
         </div >
-        <div  className='app'>
+        
+    </div >
+    <div  className='app'>
         <img src="FFC-Banner-WEB-1_JPEG.jpg" className={(menu) ? 'image1': 'image'} alt="lotus"/>
-        </div>
-    </div ><br/>
+    </div>
 
-<p style={{display: 'flex',position: 'relative',paddingTop: '410px' }}>
+{/* <p style={{display: 'flex',position: 'relative',paddingTop: '670px' }}>
 // # E10000 Queues setup:
-// If E10000 queue are not running for Cards follow below steps-</p>
+// If E10000 queue are not running for Cards follow below steps-</p> */}
 
-</>
+          <div className='board'>
+              <h2 className='contentTitle'>About</h2><br/>
+              <div className="content">Hunger kills 9 million people every year and 24,000 people every day and out of them 18,000 are children. 60% of the children in India go to sleep hungry every night. Half the children in the age group of 6 to 14 years do not have access to primary education. We feel, by subjecting children to hunger, we rob them of their God given potential. India's progress in reducing child malnutrition is very slow.</div>
+          </div>
+
+          <div className='facts'>
+                <div className='facts1'style={{width:'170px'}}><p style={{justifySelf:'centre',marginTop:'-2px',fontSize:'25px',fontWeight:'30px',marginBottom:'0px'}}>300 Crore</p><p>meals served around the world</p></div>
+                <div className='facts1' style={{backgroundColor:'#3b95e0',width:'170px'}}><p style={{justifySelf:'centre',marginTop:'-2px',fontSize:'25px',fontWeight:'30px',marginBottom:'0px'}}>11.6 Crore</p><p>meals served during COVID lockdown</p></div>
+                <div className='facts1' style={{backgroundColor:'#ae57cd'}}><p style={{justifySelf:'centre',marginTop:'-2px',fontSize:'25px',fontWeight:'30px',marginBottom:'0px'}}>54 Years</p><p>of service to humanity</p></div>
+                <div className='facts1' style={{backgroundColor:'#f29e21'}}><p style={{justifySelf:'centre',marginTop:'-2px',fontSize:'25px',fontWeight:'30px',marginBottom:'0px'}}>110</p><p>kitchens across India</p></div>
+                
+          </div>
+
+          <p className='donationFooter'>To get the receipt of donation made through NEFT, RTGS, IMPS PayTm, UPI as mentioned above, please share your legal name, postal address with pincode (and PAN if you need 80G receipt) along with transaction details on  info@iskconfoodforchild.org <br/><br/>For more information please   Call |   Whatsapp:+91-6263756519  from Monday to Saturday between 9:00am to 6:00pm </p>
+</div>
   );
 }
 
